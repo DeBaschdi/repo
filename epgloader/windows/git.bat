41944951f21ae2015c4ff70cf35865694c0258f1
DeBaschdi <46737828+DeBaschdi@users.noreply.github.com>
2019-04-17 09:52:45 +0200

    Update git.bat

--- epgloader/windows/git.bat
+++ epgloader/windows/git.bat
@@ -9,7 +9,7 @@ echo ###                    --Takealug.de feat. easyEPG Preject--               
 echo ###                                Revision 1                                           ###
 echo ###########################################################################################
 
-call "%cd%\settings\config.cmd"
+call "%cd%\config.cmd"
 echo.
 echo .
 echo ===========
